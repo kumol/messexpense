6478fed30c90f6f0fa9a5c2a1691844670df1872
kumol <kumol01763@gmail.com>
2021-09-27 01:18:51 +0600

    getting expense of a group

--- api/routes/groupexpense.js
+++ api/routes/groupexpense.js
@@ -1,11 +1,46 @@
 const route = require("express").Router();
 const GroupExpense = require("../../models/groupexpense");
 
-route.get("/",(req,res,next)=>{
-
+route.get("/", async(req,res,next)=>{
+    try{
+        let group = await GroupExpense.find({});
+        return res.status(200).json({
+            "body": {"group": group}
+        });
+    }catch(error){
+        res.status(500).json({
+            "body": {"error": error}
+        });
+    }
 });
 
-route.post('/',(req,res,next)=>{
+route.get("/:id", async(req,res,next)=>{
+    try{
+        let group = await GroupExpense.findOne({id: req.params.id});
+        return res.status(200).json({
+            "body": {"group": group}
+        });
+    }catch(error){
+        res.status(500).json({
+            "body": {"error": error}
+        });
+    }
+});
+
+route.get("/group/:id",async(req,res)=>{
+    try{
+        let group = await GroupExpense.find({group: req.params.id});
+        return res.status(200).json({
+            "body": {"group": group}
+        });
+    }catch(error){
+        res.status(500).json({
+            "body": {"error": error}
+        });
+    }
+})
+
+route.post('/', async(req,res,next)=>{
 
 });
 
--- models/groupexpense.js
+++ models/groupexpense.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 module.exports = mongoose.model("GroupExpense", mongoose.Schema({
+    id: Number,
     createdAt: Date,
     spentMoney: Number,
     date: String,
