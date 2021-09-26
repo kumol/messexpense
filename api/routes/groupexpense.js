8758466664d4b410e5ce2486d0cdc7e801ee4376
kumol <kumol01763@gmail.com>
2021-09-27 00:26:34 +0600

    group expense route structure

---
+++ api/routes/groupexpense.js
@@ -0,0 +1,13 @@
+const route = require("express").Router();
+const GroupExpense = require("../../models/groupexpense");
+
+route.get("/",(req,res,next)=>{
+
+});
+
+route.post('/',(req,res,next)=>{
+
+});
+
+
+module.exports = route;
