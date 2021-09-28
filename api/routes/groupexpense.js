e9e257e0df93bfe5686d11eb2aa2cafe2c7ea2c2
kumol <kumol01763@gmail.com>
2021-09-28 23:21:24 +0600

    group expense adding

--- api/routes/groupexpense.js
+++ api/routes/groupexpense.js
@@ -1,6 +1,6 @@
 const route = require("express").Router();
 const GroupExpense = require("../../models/groupexpense");
-
+const moment = require("moment");
 route.get("/", async(req,res,next)=>{
     try{
         let group = await GroupExpense.find({});
@@ -41,8 +41,26 @@ route.get("/group/:id",async(req,res)=>{
 })
 
 route.post('/', async(req,res,next)=>{
-
+    try{
+        let newExpense = new GroupExpense({
+            "createdAt": new Date(),
+            "spentMoney": req.body.spentMoney,
+            "user": req.body.user,
+            "date": moment().format("YYYY-MM-DD"),
+            "time": moment().format('LT'),
+            "day": moment().day("D"),
+            "year": moment().year(),
+            "month": moment().format("M"),
+            "details": req.body.details
+        }),
+        expense = await newExpense.save();
+        res.status(200).json({
+            "body":{
+                "expense": expense
+            }
+        })
+    }catch(error){
+        res.status(500).json({"error": error});
+    }
 });
-
-
 module.exports = route;
