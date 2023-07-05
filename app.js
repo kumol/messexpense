1d032ccb369e8b8765f93efe26da6ade74e958d8
_kumol <kumol@yotech.ltd>
2023-07-05 19:28:47 +0600

    user updated

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const app = express();
 const router = require('./server/api/index');
 const Sentry = require("@sentry/node");
 const Tracing = require("@sentry/tracing");
-Sentry.init({ dsn: process.env.SENTRY_DSN, integrations: [
-  new Sentry.Integrations.Http({ tracing: true }),
-  new Tracing.Integrations.Express({
-    app,
-  })
-],tracesSampleRate: 1.0,});
+Sentry.init({
+  dsn: process.env.SENTRY_DSN, integrations: [
+    new Sentry.Integrations.Http({ tracing: true }),
+    new Tracing.Integrations.Express({
+      app,
+    })
+  ], tracesSampleRate: 1.0,
+});
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
 app.use(Sentry.Handlers.errorHandler());
@@ -22,9 +24,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 app.listen(process.env.PORT, (err) => {
-    console.log("connected port " +process.env.PORT);
+  console.log("Server is running http://localhost:" + process.env.PORT);
 });
 
-app.get("/",(req,res)=>{
-    res.send("Hello everyone");
+app.get("/", (req, res) => {
+  res.send("Hello everyone");
 });
