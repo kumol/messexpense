8de7c03f8329c843a557d02c1005ad7f20e46adf
kumol <kumol01763@gmail.com>
2023-07-08 17:32:36 +0600

    group routing

--- server/api/group.route.js
+++ server/api/group.route.js
@@ -1,12 +1,20 @@
 const groupController = require("../controllers/group.controller");
+const groupexpenseController = require("../controllers/groupexpense.controller");
 
 const route = require("express").Router();
 
 route.post("/", groupController.addGroup);
 route.get("/", groupController.getGroup);
-route.get("/full_details", groupController.getFullGroup);
-route.get("/single/:id", groupController.getSingleGroup);
-route.put("/:id", groupController.updateGroup);
-route.delete("/:id", groupController.deleteGroup);
+route.get("/details", groupController.getFullGroup);
+route.get("/details/:id", groupController.getSingleGroup);
+route.put("/details/:id", groupController.updateGroup);
+route.delete("/details/:id", groupController.deleteGroup);
+
+route.get('/expense/details', groupexpenseController.getExpense);
+route.get('/expense/details/single/:id', groupexpenseController.getSingleExpense);
+route.post('/expense/details', groupexpenseController.createExpense);
+route.patch('/expense/details/:id', groupexpenseController.updateExpense);
+route.get('/expense/details/bygroup/:id', groupexpenseController.getExpenseByGroup);
+
 
 module.exports = route;
